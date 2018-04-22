3780012a45d37a0fc1d71b839d31576ce0cc156f
Denis <lordakulko@gmail.com>
2018-04-22 22:24:29 +0300

    changed hero logs

--- src/store/modules/jrpg/logs/heroHitLogs.js
+++ src/store/modules/jrpg/logs/heroHitLogs.js
@@ -2,7 +2,9 @@ export const heroHitLogs = [
   (hero, monster, damage) => `${hero.name} ударил ${monster.name} на ${damage}. `,
   (hero, monster, damage) => `${hero.name} ловок как дерево! ${monster.name} получает ${damage} урона. `,
   (hero, monster, damage) => `${hero.name} контратакует и сносит ${monster.name} ${damage} здоровья.`,
-  (hero, monster, damage) => `${hero.name} кидается на ${monster.name} и больно бьет по голове -${damage}HP.`
+  (hero, monster, damage) => `${hero.name} кидается на ${monster.name} и больно бьет по голове -${damage}HP.`,
+  (hero, monster, damage) => `${hero.name} порвал честь ${monster.name}, нещадно избивая на ${damage}HP.`,
+  (hero, monster, damage) => `${hero.name} грызанул ${monster.name} на ${damage}HP.`
 ];
 
 export const endBattleLogs = {
